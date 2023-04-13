a4eff67bdedacfe70148c2af28a223b36386c9b6
Babloo mishra <babloomshr@gmail.com>
2023-04-13 22:14:04 +0530

    Changes for 13 april class

--- 10-April-2023/utils.js
+++ 10-April-2023/utils.js
@@ -2,3 +2,10 @@
 const test = (array) =>{
     // some complex operations
 }
+
+
+//! critical comment
+//* highlighted comment
+// Todo : This is a todo comment
+//? This is a question comment
+//This is a normal comment
